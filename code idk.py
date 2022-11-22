a7dc22020a98532be54e364b94337d21917729d5
panpanfiawmak <117720042+panpanfiawmak@users.noreply.github.com>
2022-11-22 15:41:27 +0700

    Add files via upload

---
+++ code idk.py
@@ -0,0 +1,13 @@
+age = int(input("Enter your age: "))
+if age < 25:
+    print("High")
+else:
+    type_car = input("Tyoe of car: Fast Car, Family Car (fast/fam)")
+if type_car == "fam":
+            print("Low")
+else:
+    accident = input("had an accident : Yes, No (Y/N)")
+if accident == ("Y"):
+        print("high")
+else:
+        print("medium")11
---
+++ name.py
@@ -0,0 +1,3 @@
+number = int(input("Enter the number1:"))
+number2 = int(input("Enter the number2: "))
+print(number1,"+", number2, "=", number1 + number2)
