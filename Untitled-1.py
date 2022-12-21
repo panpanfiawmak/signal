8a1c93e6726f1e87be2519765555836d4a401ae0
panpanfiawmak <117720042+panpanfiawmak@users.noreply.github.com>
2022-12-21 10:10:06 +0700

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,9 @@
+question1 = input("Floats in water? (Y/N)")
+if question1 =="Y"
+  print("Fake")
+else:
+    question2 = input("Can scratch glass (Y/N)")
+    if question2 == "N"
+    print("Fake")
+    else: "Y"
+    print("could be real")
